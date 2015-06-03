d69e193da6dc02257a2293884ce2aab1b73bfcd7
tattie <tessattie@gmail.com>
2015-06-03 18:14:17 +0600

    Verification du fonctionnement des icones bootstrap

--- server.js
+++ server.js
@@ -1,25 +1,66 @@
 var express = require("express");
-var app     = express();
-var path    = require("path");
+var app = express();
+url = require('url');
 
-//Store all JS and CSS in Scripts folder.
+//Use static folders with node.js
 app.use("/css", express.static(__dirname + '/css'));
 app.use("/js", express.static(__dirname + '/js'));
+app.use("/img", express.static(__dirname + '/img'));
+app.use("/font", express.static(__dirname + '/font'));
 
 
-// Render index.html
+// Render all HTML pages
 app.get('/',function(req,res){
   res.sendFile(__dirname + '/index.html');
-  //__dirname : It will resolve to your project folder.
 });
 
-// Render chart.html
+app.get('/index.html',function(req,res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/reports.html',function(req,res){
+  res.sendFile(__dirname + '/reports.html');
+});
+
+app.get('/guidely.html',function(req,res){
+  res.sendFile(__dirname + '/guidely.html');
+});
+
 app.get('/charts.html',function(req,res){
   res.sendFile(__dirname + '/charts.html');
-  //__dirname : It will resolve to your project folder.
 });
 
-var port = Number(process.env.PORT || 3000)
-app.listen(port);
+app.get('/shortcodes.html',function(req,res){
+  res.sendFile(__dirname + '/shortcodes.html');
+});
 
+app.get('/icons.html',function(req,res){
+  res.sendFile(__dirname + '/icons.html');
+});
+
+app.get('/faq.html',function(req,res){
+  res.sendFile(__dirname + '/faq.html');
+});
+
+app.get('/pricing.html',function(req,res){
+  res.sendFile(__dirname + '/pricing.html');
+});
+
+app.get('/login.html',function(req,res){
+  res.sendFile(__dirname + '/login.html');
+});
+
+app.get('/signup.html',function(req,res){
+  res.sendFile(__dirname + '/signup.html');
+});
+
+app.get('/error.html',function(req,res){
+  res.sendFile(__dirname + '/error.html');
+});
+// End of rendering HTML pages
+
+
+// Setup of PORT value fot application to work with heroku
+var port = Number(process.env.PORT || 3000);
+app.listen(port);
 console.log("Running at Port 3000");
