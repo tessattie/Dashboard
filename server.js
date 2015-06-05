a7fdb363c53e39b7e7a4e8bd1a3bf4093837f7d2
tattie <tessattie@gmail.com>
2015-06-05 20:02:52 +0600

    Première version du widget MTO

--- server.js
+++ server.js
@@ -2,25 +2,9 @@ var express = require("express");
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var request = require('request');
 
 server.listen(process.env.PORT || 3000);
-// var request = require('request');
-// var emitter = require('events').EventEmitter;
-// var fs = require('fs');
-// var WebSocketServer = require('ws').Server
-// 	,wss = new WebSocketServer({ port: 3000 });
-
-// wss.on('connection', function connection(ws) {
-//   	ws.on('message', function incoming(message) {
-//     console.log('received: %s', message);
-// 	});
-// 	ws.send('something');
-// });
-
-// app.use(express.bodyParser());
-// app.use(express.methodOverride());
-// app.use(app.router);
-
 
 // Use static folders with node.js
 app.use("/bootstrap", express.static(__dirname + '/bootstrap'));
@@ -30,6 +14,8 @@ app.use("/font", express.static(__dirname + '/font'));
 app.use("/documentation", express.static(__dirname + '/documentation'));
 app.use("/pages", express.static(__dirname + '/pages'));
 app.use("/plugins", express.static(__dirname + '/plugins'));
+app.use("/css", express.static(__dirname + '/css'));
+app.use("/images", express.static(__dirname + '/images'));
 
 // Render all HTML pages
 app.get('/',function(req,res){
@@ -49,75 +35,31 @@ app.get('/documentation/index.html',function(req,res){
 });
 // End of rendering HTML pages
 
-// var port = Number(process.env.PORT || 3000);
-
-// var server = http.createServer(app);
-
-// server.listen(port, function(){
-// 	console.log('Server listening on port' + port);
-// });
-
-// io = require('socket.io').listen(server);
-
-
-// // Configuration pour permettre le fonctionnement sur HEROKU
-// io.use(function () {  
-//   io.set("transports", ["xhr-polling"]); 
-//   io.set("polling duration", 10); 
-// });
-
-// Quand on client se connecte, on le note dans la console
-io.sockets.on('connection', function (socket) {
-        socket.emit('message', 'Vous êtes bien connecté !');
-});
-
-// Create webSocketServer
-
-
-
-
-
-// Create server to listen to events and rendrer a response
-// var server = http.createServer(function(req, res) {
-// 	    fs.readFile(__dirname + '/index.html','utf-8', function (err, data) {
-// 	    res.writeHead(200, {'Content-Type': 'text/html'});
-// 	    res.end(data);
-// 	    });
-
-// });
-
-// // Chargement de socket.io
-// var io = require('socket.io').listen(server);
-
-
-// var port = Number(process.env.PORT || 8080);
-// server.listen(8080);
 // OpenWeatherMAp : envoi des données MTO 
-// var openweathermeteo = function(url, callback){
+var openweathermeteo = function(url, callback){
 	
-// 	request(url, function(err, response, body){
-// 		try{
-// 			var result = JSON.parse(body);
+	request(url, function(err, response, body){
+		try{
+			var result = JSON.parse(body);
 			  		
-// 			callback(null, result);
-// 		}catch(e){
-// 			callback(e); 
-// 		}
-// 	});
-// }
+			callback(null, result);
+		}catch(e){
+			callback(e); 
+		}
+	});
+}
 
-// openweathermeteo('http://api.openweathermap.org/data/2.5/weather?q=Montreuil,fr', function(err, result){
-// 	if(err) return console.log(err);
-// 	var mto = [];
-// 	mto['latitude'] = result['coord']['lat'];
-// 	mto['longitude'] = result['coord']['lon'];
-// 	mto['temp'] = result['main']['temp'];
-// 	mto['temp_min'] = result['main']['temp_min'];
-// 	mto['temp_max'] = result['main']['temp_max'];
-// 	mto['desc'] = result.weather['0']['main'];
-// });
 
-//Setup of PORT value fot application to work with heroku
-// var port = Number(process.env.PORT || 3000);
-// app.listen(port);
-// console.log("Running at Port " + port);
+// Quand on client se connecte, on lui envoi un message pour lui avertir
+io.sockets.on('connection', function (socket) {
+	openweathermeteo('http://api.openweathermap.org/data/2.5/weather?q=montreuil,fr', function(err, result){
+		if(err) return console.log(err);
+		var mto = [];
+		//mto['latitude'] = result['city']['coord']['lat'];
+		//mto['longitude'] = result['city']['coord']['lat'];
+		//mto['temp_min'] = result['list'][0]['temp']['min'];
+		//mto['temp_max'] = result['list'][0]['temp']['max'];
+		console.log(result['weather'][0].description);
+		socket.emit("meteo", {temperature : result['main']['temp'], description : result['weather'][0].description});
+		});
+});
