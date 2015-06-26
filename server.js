34d1105f023635ee37321725a3ae4c67fae8a948
tattie <tessattie@gmail.com>
2015-06-26 18:27:10 +0600

    Mise en place de la partie admin

--- server.js
+++ server.js
@@ -1,29 +1,47 @@
+// Appel des module dont on a besoin
 var express = require("express");
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var request = require('request');
 var pg = require('pg');
+var cookie = require('cookie-parser');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var sess;
 
-// var conString = "postgres://yntviqlzscnbqq:w15ISVFDrLBp414QG9sXfbHBMY@ec2-54-204-35-248.compute-1.amazonaws.com:5432/d80v1h0fvs1qi5";
-// var localString = process.env.DATABASE_URL || 'postgres://localhost:5432/?password=admin';
+// Port sur lequel écoute le serveur
 server.listen(process.env.PORT || 3000);
 
+
+// Configuration locale BDD
+var config = {};
+config.user = 'postgres';
+config.password = 'admin';
+config.database = 'dashboard';
+config.host = 'localhost';
+config.port = 5432;
+
+
+// Configuration Heroku BDD
 // var config = {};
-// config.user = 'postgres';
-// config.password = 'admin';
-// config.database = 'dashboard';
-// config.host = 'localhost';
+// config.user = 'yntviqlzscnbqq';
+// config.password = 'w15ISVFDrLBp414QG9sXfbHBMY';
+// config.database = 'd80v1h0fvs1qi5';
+// config.host = 'ec2-54-204-35-248.compute-1.amazonaws.com';
 // config.port = 5432;
 
 
-// Configuration heroku bdd
-var config = {};
-config.user = 'yntviqlzscnbqq';
-config.password = 'w15ISVFDrLBp414QG9sXfbHBMY';
-config.database = 'd80v1h0fvs1qi5';
-config.host = 'ec2-54-204-35-248.compute-1.amazonaws.com';
-config.port = 5432;
+//use cookies and sessions for administration login (cookie declaration comes first)
+app.use(cookie());
+app.use(session({secret:"anystringoftext",
+				 saveUninitialized:true,
+				 resave:true}));
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 // Use static folders with node.js
 app.use("/bootstrap", express.static(__dirname + '/bootstrap'));
@@ -37,26 +55,63 @@ app.use("/css", express.static(__dirname + '/css'));
 app.use("/assets", express.static(__dirname + '/assets'));
 app.use("/admin", express.static(__dirname + '/admin'));
 app.use("/js", express.static(__dirname + '/js'));
+// End of use of static folders
 
 // Render all HTML pages
 app.get('/',function(req,res){
+  sess=req.session;
   res.sendFile(__dirname + '/index.html');
 });
-
 app.get('/index.html',function(req,res){
+  sess=req.session;
   res.sendFile(__dirname + '/index.html');
 });
-
-app.get('/pages/calendar.html',function(req,res){
-  res.sendFile(__dirname + '/pages/calendar.html');
-});
-
-app.get('/documentation/index.html',function(req,res){
-  res.sendFile(__dirname + '/documentation/index.html');
-});
-
 app.get('/admin',function(req,res){
+  sess=req.session;
   res.sendFile(__dirname + '/admin/login.html');
+});
+app.get('/admin/lescommentaires',function(req,res){
+  sess=req.session;
+  if(sess.userid)
+  {
+  	res.sendFile(__dirname + '/admin/commentaires.html');
+  }
+  else
+  {
+  	res.sendFile(__dirname + '/admin/login.html');
+  }
+});
+app.get('/admin/lesliens',function(req,res){
+  sess=req.session;
+  if(sess.userid)
+  {
+  	res.sendFile(__dirname + '/admin/liens.html');
+  }
+  else
+  {
+  	res.sendFile(__dirname + '/admin/login.html');
+  }
+});
+app.post('/admin/traitement', function(req,res){
+	sess=req.session;
+	pg.connect(config, function(err, client, done) {
+			if (err)
+				console.log(err); 
+			else 
+				client.query("SELECT * FROM login WHERE username = ($1) AND mdp = ($2)",[req.body.username,req.body.mdp], function(err, rows){
+					 if(rows['rowCount'] > 0)
+					 {
+					 	//sess.username = rows['rows'].username;
+					 	sess.userid = req.body.username;
+					 	res.sendFile(__dirname + '/admin/administration.html');
+					 }
+					 else
+					 {
+					 	res.sendFile(__dirname + '/admin/login.html');
+					 }
+					 client.end();
+				}); 
+		  });
 });
 // End of rendering HTML pages
 
@@ -74,16 +129,12 @@ var openweathermeteo = function(url, callback){
 	});
 }
 
-// Quand on client se connecte, on lui envoi un message pour lui avertir
+// Quand un client se connecte, on lui envoi les infos de la page principale
 io.sockets.on('connection', function (socket) {
 	openweathermeteo('http://api.openweathermap.org/data/2.5/weather?q=montreuil,fr', function(err, result){
 		if(err) return console.log(err);
 		var mto = [];
-		//mto['latitude'] = result['city']['coord']['lat'];
-		//mto['longitude'] = result['city']['coord']['lat'];
 		mto['temp'] = result['main']['temp'];
-		//mto['temp_min'] = result['list'][0]['temp']['min'];
-		//mto['temp_max'] = result['list'][0]['temp']['max'];
 		mto['desc'] = result['weather'][0].description;
 		console.log(mto['desc']);
 		socket.emit("meteo", {temperature : mto['temp'], description : mto['desc']});
@@ -96,8 +147,20 @@ io.sockets.on('connection', function (socket) {
 			else 
 				client.query("SELECT * FROM messages WHERE slider = true", function(err, rows){
 					messages_affiche.push(rows);
-					console.log(messages_affiche[0].rows[0].title);
 					socket.emit("messages_affiches", messages_affiche[0].rows);
+					 client.end();
+				}); 
+		  });
+
+		var liens_affiches = [];
+		pg.connect(config, function(err, client, done) {
+			if (err)
+				console.log(err); 
+			else 
+				client.query("SELECT * FROM liens", function(err, rows){
+					liens_affiches.push(rows);
+					console.log(liens_affiches[0].rows);
+					socket.emit("liens_affiches", liens_affiches[0].rows);
 					 client.end();
 				}); 
 		  });
@@ -111,15 +174,24 @@ io.sockets.on('connection', function (socket) {
 					console.log(err);
 					client.end();
 				}); 
-				
 		  });
 	});
 
-	socket.on('authentification', function (data) {
-		if((data['username']=='Simplon')&&(data['password']=='Dashboard'))
-		{
-			console.log("Vous avez le bon mot de passe");
-		}
+	socket.on('new_link', function (data){
+		pg.connect(config, function(err, client, done) {
+			if (err)
+			{
+				console.log(err); 
+			}
+			else 
+			{
+				client.query('INSERT INTO liens(lien, lienlabel, description) VALUES($1,$2, $3)',[
+					data['link'], data['label'], data['desc']], function(err){
+					console.log(err);
+					client.end();
+				});
+			}
+		  });
 	});
 
 	socket.on('supprimer_commentaires', function (data){
@@ -134,6 +206,27 @@ io.sockets.on('connection', function (socket) {
 				for(var i = 0; i < data.length; i++)
 				{
 					client.query('DELETE FROM commentaires WHERE id ='+data[i], function(err){
+						console.log(err);
+						client.end();
+					}); 
+				};
+				socket.emit("reload", "reload");
+			}
+		  });
+	});
+
+	socket.on('supprimer_liens', function (data){
+		pg.connect(config, function(err, client, done) {
+			if (err)
+			{
+				console.log(err); 
+				client.end();
+			}
+			else 
+			{
+				for(var i = 0; i < data.length; i++)
+				{
+					client.query('DELETE FROM liens WHERE id ='+data[i], function(err){
 						console.log(err);
 						client.end();
 					}); 
@@ -162,38 +255,16 @@ io.sockets.on('connection', function (socket) {
 				console.log(err); 
 			else 
 				client.query("SELECT * FROM commentaires", function(err, rows){
+					if(err)
+					{
+						console.log(err);
+					}
 					var commentaires = [];
 					commentaires.push(rows);
-					console.log(err);
 					socket.emit("commentaires", commentaires[0].rows);
 					client.end();
 				});
 		  });
+	});
 
-	// socket.on('nouvelle_promo', function (nouvelle_promo){
-	// 	var today = new Date();
-	// 	var dd = today.getDate();
-	// 	var mm = today.getMonth()+1; //January is 0!
-	// 	var yyyy = today.getFullYear();
-
-	// 	if(dd<10) {
-	// 	    dd='0'+dd
-	// 	} 
-
-	// 	if(mm<10) {
-	// 	    mm='0'+mm
-	// 	} 
-
-	// 	var current_date = yyyy+'/'+mm+'/'+dd;
-	// 	pg.connect(config, function(err, client, done) {
-	// 		if (err)
-	// 			console.log(err); 
-	// 		else 
-	// 			client.query('INSERT INTO promos(numero, lieu, date) VALUES($1,$2,$3)',[nouvelle_promo['numero_promo'],
-	// 				nouvelle_promo['lieu_promo'], current_date], function(err){
-	// 				console.log(err);
-	// 			}); 
-	// 	  });
-	// 		socket.emit("confirmation_nouvelle_promo", "Vous avez créé une nouvelle promo!");
-	// 	});
-});
+	
